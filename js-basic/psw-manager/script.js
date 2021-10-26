1c517d148b4af0812818c80da91fb7e2cb4e1016
olesien <olesien2@gmail.com>
2021-10-26 15:20:14 +0200

    added pswchecker v2

--- js-basic/psw-manager/script.js
+++ js-basic/psw-manager/script.js
@@ -4,7 +4,7 @@
  * Link: https://teams.microsoft.com/l/message/19:ee60e4662a5a4a95babd494649401fdc@thread.tacv2/1634737231314?tenantId=1f14a049-d614-4ea3-8d0d-040443a8bc19&groupId=10a26e5b-f1f9-4c41-892c-ba5f3e43d8ba&parentMessageId=1634737231314&teamName=FED21M&channelName=JavaScript%20Grundkurs-%2065%20Yhp&createdTime=1634737231314
  */
 
-let password;
+let passwords = 'secretAAAA';
 //password = "password"; // inte giltigt
 //password = "p@ssw%rd"; // giltigt
 //password = "pa$$word"; // giltigt
