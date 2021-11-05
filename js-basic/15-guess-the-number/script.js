8074dd20eb4c20e36637c363951b39e7e3d9e8d1
olesien <olesien2@gmail.com>
2021-11-05 10:39:32 +0100

    hi

--- js-basic/15-guess-the-number/script.js
+++ js-basic/15-guess-the-number/script.js
@@ -31,16 +31,26 @@ const formEl = document.getElementById("guesser");
 const numberInputEl = document.getElementById("number-input");
 const feedbackEl = document.getElementById("feedback");
 const resetButtonEl = document.getElementById("reset-button");
+const highscoreEl = document.getElementById("highscore");
+const guessesEl = document.getElementById("guesses");
 
 // Get a random number between 1-10
 function getRandomNumber() {
-    return Math.ceil(Math.random() * 10);
+	return Math.ceil(Math.random() * 10);
 }
 
-function reset() {
-    feedbackEl.innerText = "Guess a number between 1 and 10!";
-    numberInputEl.value = "";
-    numberToGuess = parseInt(getRandomNumber(50));
+function reset(resetCurrent = true) {
+	if (resetCurrent) {
+		feedbackEl.innerText = "Guess a number between 1 and 10!";
+		numberInputEl.value = "";
+	}
+
+	guesses = 0;
+	//highscore = 0;
+	guessesEl.innerText = guesses;
+	//highscoreEl.value = highscore;
+	numberToGuess = parseInt(getRandomNumber());
+	resetOnNext = false;
 }
 
 // const getRandomNumber = function(max = 10) {
@@ -56,65 +66,73 @@ let highscore = false;
 //while (!exitGame) {
 // PARTY ON
 
-let numberToGuess = parseInt(getRandomNumber(50));
+let numberToGuess = parseInt(getRandomNumber());
 //let continueGame = true;
 let guesses = 0;
+let resetOnNext = true;
 
 console.log(`numberToGuess: ${numberToGuess}`);
 
 //while (continueGame) {
 // Increase number of guesses made
 formEl.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log(e);
-    //console.log(e.get("number-input"));
-    //console.log(form.get("number-input"));
+	e.preventDefault();
+	if (resetOnNext) {
+		reset(false);
+	}
+	console.log(e);
+	//console.log(e.get("number-input"));
+	//console.log(form.get("number-input"));
 
-    guesses++; //   guesses += 1   guesses = guesses + 1
+	guesses++; //   guesses += 1   guesses = guesses + 1
+	guessesEl.innerText = guesses;
 
-    // Ask user for guess
-    let guess = Number(numberInputEl.value);
-    if (isNaN(guess) || guess < 1 || guess > 10) {
-        feedbackEl.innerText = "Invalid Input";
-        return;
-    }
-    console.log("Guessed number:", guess, typeof guess);
+	// Ask user for guess
+	let guess = Number(numberInputEl.value);
+	if (isNaN(guess) || guess < 0 || guess > 10) {
+		feedbackEl.innerText = "Invalid Input";
+		return;
+	}
+	console.log("Guessed number:", guess, typeof guess);
 
-    // Is guess correct?
-    if (guess === numberToGuess) {
-        // we can haz highscore?
-        if (highscore) {
-            // new highscore?
-            if (guesses < highscore) {
-                console.log(`YAY NEW HIGHSCORE!`);
-                highscore = guesses;
-            } else {
-                feedbackEl.innerText = `Sorry, no new highscore. Your current highscore is ${highscore}.`;
+	// Is guess correct?
+	if (guess === numberToGuess) {
+		// we can haz highscore?
+		if (highscore) {
+			// new highscore?
+			if (guesses < highscore) {
+				console.log(`YAY NEW HIGHSCORE!`);
+				highscore = guesses;
+			} else {
+				feedbackEl.innerText = `Sorry, no new highscore. Your current highscore is ${highscore}.`;
+				resetOnNext = true;
 
-                return;
-            }
-        } else {
-            highscore = guesses;
-        }
+				return;
+			}
+		} else {
+			highscore = guesses;
+			highscoreEl.innerText = guesses;
+		}
 
-        console.log("Guess was correct! 🥳");
-        feedbackEl.innerText = `Great success! You guessed the correct answer in ${guesses} guesses 🥳`;
+		console.log("Guess was correct! 🥳");
+		feedbackEl.innerText = `Great success! You guessed the correct answer in ${guesses} guesses 🥳`;
+		resetOnNext = true;
 
-        //continueGame = false;
-    } else if (guess === 0) {
-        console.log("Guess was 0, quitting game");
-        alert(`Y U GIVE UP AFTER ONLY ${guesses} GUESSES?!`);
-        //continueGame = false;
-        //exitGame = true;
-    } else if (guess < numberToGuess) {
-        feedbackEl.innerText = `TOO LOW!`;
-    } else if (guess > numberToGuess) {
-        feedbackEl.innerText = `TOO HIGH!`;
-    }
+		//continueGame = false;
+	} else if (guess === 0) {
+		console.log("Guess was 0, quitting game");
+		feedbackEl.innerText = `Y U GIVE UP AFTER ONLY ${guesses} GUESSES?!`;
+		//continueGame = false;
+		//exitGame = true;
+	} else if (guess < numberToGuess) {
+		feedbackEl.innerText = `TOO LOW!`;
+	} else if (guess > numberToGuess) {
+		feedbackEl.innerText = `TOO HIGH!`;
+	}
 });
 
 resetButtonEl.addEventListener("click", () => {
-    reset();
+	reset();
 });
 //}
 //}
