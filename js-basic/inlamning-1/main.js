c2b1c60a8d5215e6902065ce1a7774cb5f947404
olesien <olesien2@gmail.com>
2021-11-13 15:07:07 +0100

    shit works at least

--- js-basic/inlamning-1/main.js
+++ js-basic/inlamning-1/main.js
@@ -6,14 +6,59 @@ const choices = [
     { name: "Sarah", url: "./images/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" },
 ];
 
+let choicesButtonsCount = 5;
+
 let randomizedChoice = -1;
 
 const toggleGameScreen = () => {
     if (!game.classList.contains("showElement")) {
         //start the game
+<<<<<<< Updated upstream
         game.querySelector("img").src =
             "./images/07CAT-STRIPES-mediumSquareAt3X-v2.jpg";
         
+=======
+        randomizedChoice = getRandomInt(choices.length);
+        console.log(choices[randomizedChoice]);
+        game.querySelector("img").src = choices[randomizedChoice].url;
+
+        //USE HTML, NEEDS TO RERENDER!
+        // let choiceButtonArray = Array.from(
+        //     game.getElementsByClassName("choice")
+        // );
+
+        let choicesCopy = choices;
+
+        // let randomizedPicks = choiceButtonArray.forEach(choice => {
+
+        // })
+
+        let placementIndex = getRandomInt(choicesButtonsCount);
+        choicesEl.innerHTML = "";
+
+        for (let index = 0; index < choicesButtonsCount; index++) {
+            let name = "";
+            if (placementIndex === index) {
+                name = choices[randomizedChoice].name;
+            } else {
+                let filteredChoices = choicesCopy.filter(
+                    (choice) =>
+                        !choice?.picked == true &&
+                        choice.name !== choices[randomizedChoice].name
+                );
+                console.log(filteredChoices);
+                let randomChoice = getRandomInt(filteredChoices.length);
+                filteredChoices[randomChoice].picked = true;
+                name = filteredChoices[randomChoice].name;
+                choicesCopy = filteredChoices;
+            }
+            choicesEl.innerHTML += `<li class="choice">
+            <input type="button" value="${name}" class="choiceButton" id="choice-${
+                index + 1
+            }">
+        </li>`;
+        }
+>>>>>>> Stashed changes
     }
     introduction.classList.toggle("hideElement");
     game.classList.toggle("showElement");
