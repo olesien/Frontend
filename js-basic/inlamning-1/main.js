70b2e3cf1d55bc36c8a153b858e4eff738b47767
olesien <olesien2@gmail.com>
2021-11-15 00:04:31 +0100

    Fixed t

--- js-basic/inlamning-1/main.js
+++ js-basic/inlamning-1/main.js
@@ -1,75 +1,98 @@
 const gameContainer = document.querySelector("#gameContainer");
 const introduction = document.querySelector(".introduction");
 const game = document.querySelector(".game");
+//const choiceButtonEl = document.querySelector(".choiceButton");
+const choicesEl = document.querySelector("#choices");
 
 const choices = [
     { name: "Sarah", url: "./images/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" },
+    { name: "Adam", url: "./images/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" },
+    { name: "Robin", url: "./images/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" },
+    { name: "Tivi", url: "./images/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" },
+    { name: "Samuel", url: "./images/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" },
+    { name: "Jessica", url: "./images/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" },
+    { name: "Rick", url: "./images/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" },
 ];
 
-let choicesButtonsCount = 5;
+let choicesEditable = choices;
 
 let randomizedChoice = -1;
-
-const toggleGameScreen = () => {
-    if (!game.classList.contains("showElement")) {
-        //start the game
-<<<<<<< Updated upstream
-        game.querySelector("img").src =
-            "./images/07CAT-STRIPES-mediumSquareAt3X-v2.jpg";
-        
-=======
-        randomizedChoice = getRandomInt(choices.length);
-        console.log(choices[randomizedChoice]);
-        game.querySelector("img").src = choices[randomizedChoice].url;
-
-        //USE HTML, NEEDS TO RERENDER!
-        // let choiceButtonArray = Array.from(
-        //     game.getElementsByClassName("choice")
-        // );
-
-        let choicesCopy = choices;
-
-        // let randomizedPicks = choiceButtonArray.forEach(choice => {
-
-        // })
-
-        let placementIndex = getRandomInt(choicesButtonsCount);
-        choicesEl.innerHTML = "";
-
-        for (let index = 0; index < choicesButtonsCount; index++) {
-            let name = "";
-            if (placementIndex === index) {
-                name = choices[randomizedChoice].name;
-            } else {
-                let filteredChoices = choicesCopy.filter(
-                    (choice) =>
-                        !choice?.picked == true &&
-                        choice.name !== choices[randomizedChoice].name
-                );
-                console.log(filteredChoices);
-                let randomChoice = getRandomInt(filteredChoices.length);
-                filteredChoices[randomChoice].picked = true;
-                name = filteredChoices[randomChoice].name;
-                choicesCopy = filteredChoices;
-            }
-            choicesEl.innerHTML += `<li class="choice">
-            <input type="button" value="${name}" class="choiceButton" id="choice-${
-                index + 1
-            }">
-        </li>`;
-        }
->>>>>>> Stashed changes
-    }
-    introduction.classList.toggle("hideElement");
-    game.classList.toggle("showElement");
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
 };
 
-const checkGuess = (clickedEl, id) => {};
+const toggleGameScreen = () => {
+    //start the game
+    randomizedChoice = getRandomInt(choicesEditable.length);
+    console.log(choicesEditable[randomizedChoice]);
+    game.querySelector("img").src = choices[randomizedChoice].url;
+
+    //USE HTML, NEEDS TO RERENDER!
+    let choiceArray = Array.from(game.getElementsByClassName("choice"));
+
+    let choicesCopy = choicesEditable;
+
+    // let randomizedPicks = choiceButtonArray.forEach(choice => {
+
+    // })
+
+    let placementIndex = getRandomInt(choiceArray.length);
+    choicesEl.innerHTML = "";
+
+    const choiceElementMap = choiceArray.map((choiceEl, index) => {
+        let name = "";
+        if (placementIndex === index) {
+            name = choices[randomizedChoice].name;
+        } else {
+            let filteredChoices = choicesCopy.filter(
+                (choice) =>
+                    !choice?.picked == true &&
+                    choice.name !== choicesEditable[randomizedChoice].name
+            );
+            console.log(filteredChoices);
+            let randomChoice = getRandomInt(filteredChoices.length);
+            console.log(filteredChoices[randomChoice]);
+            filteredChoices[randomChoice].picked = true;
+
+            name = filteredChoices[randomChoice].name;
+            choicesCopy = filteredChoices;
+            console.log("Name: " + name);
+        }
+        choiceEl.innerHTML = `
+            <input type="button" value="${name}" class="choiceButton" id="choice-${
+            index + 1
+        }">`;
+        return choiceEl;
+    });
+    choiceElementMap.forEach((choice) => choicesEl.appendChild(choice));
+
+    //choiceElementMap.forEach()
+};
+
+const checkGuess = (clickedEl, id) => {
+    if (clickedEl.value === choicesEditable[randomizedChoice].name) {
+        alert("correct");
+        //do next round
+        console.log(
+            choicesEditable.filter((choice) => choice.name !== clickedEl.value)
+        );
+        choicesEditable = choicesEditable.filter(
+            (choice) => choice.name !== clickedEl.value
+        );
+
+        toggleGameScreen();
+    } else if (!clickedEl.classList.contains("incorrect-guess")) {
+        alert("incorrect");
+        clickedEl.classList.add("incorrect-guess");
+    }
+};
 
 gameContainer.addEventListener("click", (e) => {
     const clickedEl = e.target;
     if (clickedEl.classList.contains("startGame")) {
         toggleGameScreen();
+        introduction.classList.toggle("hideElement");
+        game.classList.toggle("showElement");
         return;
     }
     if (clickedEl.classList.contains("choiceButton")) {
