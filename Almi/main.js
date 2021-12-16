b6a2b44e952feba3fd06e7eba2d7f50ae6787b11
olesien <olesien2@gmail.com>
2021-12-16 18:27:24 +0100

    Added subnavs

--- Almi/main.js
+++ Almi/main.js
@@ -8,6 +8,9 @@ const payList = document.querySelectorAll(".pay");
 
 const backToTopBtn = document.querySelector("#back-to-top");
 
+const navbar = document.querySelector(".navbar");
+const linkContainer = document.querySelectorAll(".link-container");
+
 hamburgerBtn.addEventListener("click", () => {
     logo.id = "hide-logo";
     hideNavBtn.id = "show-hide-nav";
@@ -38,4 +41,32 @@ options.forEach((option, index) => {
 
 backToTopBtn.addEventListener("click", () => {
     document.documentElement.scrollTop = 0;
+});
+
+navbar.addEventListener("click", (e) => {
+    console.log(e.target.classList.forEach((e) => console.log(e)));
+    if (e.target.classList.contains("link-container")) {
+        //right nav
+
+        let isShown = false;
+        if (e.target.classList.contains("show")) {
+            isShown = true;
+        }
+
+        linkContainer.forEach((link) => {
+            link.classList.remove("show");
+        });
+        if (!isShown) {
+            e.target.classList.add("show");
+        }
+    }
+
+    if (
+        e.target.classList.contains("subnav") ||
+        e.target.classList.contains("fa-chevron-down")
+    ) {
+        linkContainer.forEach((link) => {
+            link.classList.remove("show");
+        });
+    }
 });
