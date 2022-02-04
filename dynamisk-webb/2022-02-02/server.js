67ba2239cc64804c1b1114c4caa97444bc8a2150
olesien <olesien2@gmail.com>
2022-02-04 11:38:20 +0100

    added views

--- dynamisk-webb/2022-02-02/server.js
+++ dynamisk-webb/2022-02-02/server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-
+const _ = require("lodash");
+const morgan = require("morgan");
 //joke data
-const rawdata = fs.readFileSync("./data/oneliners.json");
-const jokes = JSON.parse(rawdata);
+// const rawdata = fs.readFileSync("./data/oneliners.json");
+// const jokes = JSON.parse(rawdata);
+const jokes = require("./data/oneliners.json");
 
 const app = express();
+
+//middleware
+// app.use((req, res, next) => {
+//     //console.log(req.method, req.url);
+//     console.log(`Incoming ${req.method} request for ${req.url}`);
+//     next();
+// });
+
+app.use(morgan("dev"));
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
@@ -30,11 +41,14 @@ app.get("/jokes", (req, res) => {
     // console.log(req.method, req.url);
     // res.send("<h1>hi</h1>");
     //res.get('./pages/index.html');
-    res.send(
-        jokes.sort(function () {
-            return 0.5 - Math.random();
-        })[0]
-    );
+    const i = _.random(0, jokes.length - 1);
+    // res.send(
+    //     jokes.sort(function () {
+    //         return 0.5 - Math.random();
+    //     })[0]
+    // );
+    //res.send(jokes[i]);
+    res.render("jokes", { joke: jokes[i] });
 });
 
 // app.get("/api/nom", (req, res) => {
@@ -42,10 +56,19 @@ app.get("/jokes", (req, res) => {
 //     res.send({ msg: "Cakes are nom-nom-nom." });
 // });
 
-app.get("*", (req, res) => {
-    console.log("wrong page!");
-    res.send("Error, wrong page!");
+// app.get("*", (req, res) => {
+//     console.log("wrong page!");
+//     res.send("Error, wrong page!");
+// });
+
+app.use((req, res, next) => {
+    res.send("Sorry, we could not find that page! :(");
 });
+
+// app.use(
+// express.static("public")
+
+// );
 
 app.listen(3000, () => {
     console.log("Server has been started at port 3000!");
