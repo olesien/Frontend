053b44fda7493f7ca9de5e710ab0ee15a9f668ef
olesien <olesien2@gmail.com>
2022-02-11 11:46:24 +0100

    tested file uploads

--- dynamisk-webb/2022-02-11/fileupload.js
+++ dynamisk-webb/2022-02-11/fileupload.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const app = express();
 
+const multer = require("multer");
+
 app.use(express.static("static"));
+
+// const urlenconded = express.urlencoded({ extended: false });
+
+// const jsonencoder = express.json();
+
+const storageObject = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, (Math.random() + 1).toString(36).substring(7) + ".png");
+    },
+});
+
+const multipartDataEncoder = multer({ storage: storageObject });
+
+//app.use(urlenconded);
+
+app.post("/upload", multipartDataEncoder.single("myFile"), (req, res) => {
+    console.log(req.file);
+    res.send(req.file);
+});
 
 app.listen(3000, () => {
     console.log("Server started at 3000");
