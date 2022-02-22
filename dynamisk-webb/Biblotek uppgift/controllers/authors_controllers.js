6bebd703d88d48aefd0f73558cdbdcd36abf602a
olesien <olesien2@gmail.com>
2022-02-22 11:33:20 +0100

    Added new library

--- dynamisk-webb/Biblotek uppgift/controllers/authors_controllers.js
+++ dynamisk-webb/Biblotek uppgift/controllers/authors_controllers.js
@@ -2,97 +2,96 @@ const authors = require("../models/authors");
 const log = require("../logging");
 
 const create = async (req, res) => {
-    try {
-        log.debug("CREATE", req.body);
-        let card = await new authors(req.body).save();
-        log.info("author successfully saved" + JSON.stringify(card));
-        return res.status(201).send({ success: true, data: { card } });
-    } catch (err) {
-        log.error("author_create" + JSON.stringify(err));
-        return res.status(500).send({ success: false, data: err });
-    }
+	log.debug("upper create req");
+	try {
+		log.debug("CREATE AUTHOR START");
+		let card = await new authors(req.body).save();
+		log.info("author successfully saved" + JSON.stringify(card));
+		return res.status(201).send({ success: true, data: { card } });
+	} catch (err) {
+		log.error("author_create" + JSON.stringify(err));
+		return res.status(500).send({ success: false, data: err });
+	}
 };
 
 const read = async (req, res) => {
-    try {
-        let card;
+	try {
+		let card;
 
-        if (req.params.id) {
-            card = await authors.where({ id: req.params.id }).fetch({
-                require: false,
-                withRelated: ["titles"],
-            });
-            log.info("One author fetched" + JSON.stringify(card));
-        } else {
-            card = await authors.fetchAll();
-            log.info("All authors fetched" + JSON.stringify(card));
-        }
+		if (req.params.id) {
+			card = await authors.where({ id: req.params.id }).fetch({
+				require: false,
+				withRelated: ["titles"],
+			});
+			log.info("One author fetched" + JSON.stringify(card));
+		} else {
+			card = await authors.fetchAll();
+			log.info("All authors fetched" + JSON.stringify(card));
+		}
 
-        if (!card) {
-            log.info("author(s) not found");
-            return res.status(400).send({
-                success: false,
-                data: "Not found",
-            });
-        }
-        return res.status(200).send({ success: true, data: { card } });
-    } catch (err) {
-        log.error("author_read" + JSON.stringify(err));
-        return res.status(500).send({ success: false, data: err });
-    }
+		if (!card) {
+			log.info("author(s) not found");
+			return res.status(400).send({
+				success: false,
+				data: "Not found",
+			});
+		}
+		return res.status(200).send({ success: true, data: { card } });
+	} catch (err) {
+		log.error("author_read" + JSON.stringify(err));
+		return res.status(500).send({ success: false, data: err });
+	}
 };
 
 const update = async (req, res) => {
-    try {
-        log.debug("UPDATE", req.body);
-        //let card = new authors(req.body).save();
-        let card = await authors.where({ id: req.params.id }).fetch({
-            require: false,
-        });
-        card = await card.set(req.body).save();
+	try {
+		log.debug("UPDATE", req.body);
+		//let card = new authors(req.body).save();
+		let card = await authors.where({ id: req.params.id }).fetch({
+			require: false,
+		});
+		card = await card.set(req.body).save();
 
-        log.info("Updated author, " + JSON.stringify(card));
+		log.info("Updated author, " + JSON.stringify(card));
 
-        return res.status(201).send({ success: true, data: { card } });
-    } catch (err) {
-        log.error("author_update" + JSON.stringify(err));
-        return res.status(500).send({ success: false, data: err });
-    }
+		return res.status(201).send({ success: true, data: { card } });
+	} catch (err) {
+		log.error("author_update" + JSON.stringify(err));
+		return res.status(500).send({ success: false, data: err });
+	}
 };
 
 const del = async (req, res) => {
-    try {
-        let card = await authors.where({ id: req.params.id }).fetch();
+	try {
+		let card = await authors.where({ id: req.params.id }).fetch();
 
-        await card.destroy();
-        // .then((collection) => {
-        // 	console.log(collection.toJSON());
-        // 	// Loopa igenom alla resultaten
-        // 	collection.forEach(async (model) => {
-        // 		// Radera dem en efter en
-        // 		await deleteBattle(req.params.id);
+		await card.destroy();
+		// .then((collection) => {
+		// 	console.log(collection.toJSON());
+		// 	// Loopa igenom alla resultaten
+		// 	collection.forEach(async (model) => {
+		// 		// Radera dem en efter en
+		// 		await deleteBattle(req.params.id);
 
-        // 		model.destroy().then();
-        // 	});
-        // });
+		// 		model.destroy().then();
+		// 	});
+		// });
 
-        if (!card) {
-            log.info(`Author ID ${req.params.id} NOT deleted, not found`);
-            return res.status(400).send({
-                success: false,
-                data: "Not found",
-            });
-        }
-        log.info(
-            `Author ID ${req.params.id} deleted, return data: ${JSON.stringify(
-                card
-            )}`
-        );
-        return res.status(200).send({ success: true, data: { card } });
-    } catch (err) {
-        log.error("author_del" + JSON.stringify(err));
-        return res.status(500).send({ success: false, data: err });
-    }
+		if (!card) {
+			log.info(`Author ID ${req.params.id} NOT deleted, not found`);
+			return res.status(400).send({
+				success: false,
+				data: "Not found",
+			});
+		}
+		log.info(
+			`Author ID ${req.params.id} deleted, return data: ${JSON.stringify(card)}`
+		);
+		return res.status(200).send({ success: true, data: { card } });
+	} catch (err) {
+		log.error("author_del" + JSON.stringify(err));
+		return res.status(500).send({ success: false, data: err });
+	}
 };
 
 module.exports = { read, create, update, del };
--- dynamisk-webb/Biblotek uppgift/routes/authors_routes.js
+++ dynamisk-webb/Biblotek uppgift/routes/authors_routes.js
@@ -1,6 +1,7 @@
 // const { Model } = require("bookshelf");
 const express = require("express");
 const router = express.Router();
+const log = require("../logging");
 
 const authors_controllers = require("../controllers/authors_controllers");
 
@@ -8,14 +9,17 @@ router.get("/", authors_controllers.read);
 
 router.get("/:id", authors_controllers.read);
 
-router.post("/", authors_controllers.create);
+router.post("/", (req, res) => {
+	log.debug("invoked /: post on author");
+	authors_controllers.create(req, res);
+});
 
 router.put("/:id", (req, res) => {
-    res.send("PUT (" + req.params.id + ") not yet implemented");
+	res.send("PUT (" + req.params.id + ") not yet implemented");
 });
 
 router.delete("/:id", (req, res) => {
-    res.send("DELETE (" + req.params.id + ") not yet implemented");
+	res.send("DELETE (" + req.params.id + ") not yet implemented");
 });
 
 module.exports = router;
