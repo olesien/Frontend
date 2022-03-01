9676a4a907706e1ffaba3c68f71ab4ab9e3ed3d2
olesien <olesien2@gmail.com>
2022-03-01 10:41:54 +0100

    changes 2
    2

--- dynamisk-webb/06-books/routes/index.js
+++ dynamisk-webb/06-books/routes/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
 
-const registerController = require("../controllers/register_controller");
+// const registerController = require("../controllers/register_controller");
+const authController = require("../controllers/auth_controller");
 const userValidationRules = require("../validation/user");
 /* GET / */
 router.get("/", (req, res, next) => {
-    res.send({ success: true, data: { msg: "oh, hi" } });
+	res.send({ success: true, data: { msg: "oh, hi" } });
 });
 
 router.use("/authors", require("./authors"));
@@ -18,8 +19,8 @@ router.use("/profile", auth.basic, require("./profile"));
 //register new user
 
 router.post(
-    "/register",
-    userValidationRules.createRules,
-    registerController.register
+	"/register",
+	userValidationRules.createRules,
+	authController.register
 );
 module.exports = router;
