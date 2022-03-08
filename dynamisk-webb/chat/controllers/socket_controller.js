b47cf4d59b1634ae898612cd47eb75c160a50b9b
olesien <olesien2@gmail.com>
2022-03-08 15:01:30 +0100

    Added chat functionality

--- dynamisk-webb/chat/controllers/socket_controller.js
+++ dynamisk-webb/chat/controllers/socket_controller.js
@@ -2,5 +2,32 @@ const debug = require("debug")("chat:socket_controller");
 
 module.exports = function (socket) {
 	debug("A new client has connected", socket.id);
+
 	socket.emit("welcome", "welcome to chat 3000");
+	socket.broadcast.emit("chat-welcome", {
+		user: socket.id,
+		message: "Joined the chat",
+	});
+	socket.on("disconnect", function () {
+		debug(`Client ${this.id} disconnected :(`);
+		socket.broadcast.emit("chat-goodbye", {
+			user: this.id,
+			message: "Left the chat",
+		});
+	});
+	socket.on("chat-message", function (data) {
+		debug("Someone did something", data.message);
+		this.emit("chat-message", { user: "You", message: data.message });
+		this.broadcast.emit("chat-message", {
+			user: data.user,
+			message: data.message,
+		});
+	});
+	socket.on("chat-typing", function (data) {
+		//debug("Someone did something", data.message);
+		this.broadcast.emit("chat-typing", {
+			user: data.user,
+			message: data.message,
+		});
+	});
 };
--- dynamisk-webb/chat/public/assets/js/chat.js
+++ dynamisk-webb/chat/public/assets/js/chat.js
@@ -1,10 +1,55 @@
+const messagesEl = document.querySelector("#messages");
+const typingEl = document.querySelector("#typing");
+
 const messageForm = document.querySelector("#message-form");
 const messageEl = document.querySelector("#message");
+const userEl = document.querySelector("#user");
 
 const socket = io();
-
+const addMessageToChat = (data) => {
+	const liEl = document.createElement("li");
+	liEl.innerText = `${data.user}: ${data.message}`;
+	messagesEl.appendChild(liEl);
+};
 socket.on("welcome", (data) => {
 	console.log("A welcome message was received", data);
+});
+
+socket.on("chat-welcome", (data) => {
+	addMessageToChat(data);
+});
+
+socket.on("chat-goodbye", (data) => {
+	addMessageToChat(data);
+});
+
+socket.on("chat-typing", (data) => {
+	//addMessageToChat(data);
+	console.log(typingEl.querySelectorAll("#" + data.user));
+	if (typingEl.querySelectorAll("#" + data.user).length === 0) {
+		const liEl = document.createElement("li");
+		liEl.innerText = `${data.user} ${data.message}`;
+		liEl.id = data.user;
+		typingEl.appendChild(liEl);
+		setTimeout(() => {
+			liEl.remove();
+		}, 5000);
+	}
+});
+
+socket.on("chat-message", (data) => {
+	console.log(data);
+	addMessageToChat(data);
+});
+
+messageEl.addEventListener("keyup", () => {
+	if (!userEl.value) {
+		return;
+	}
+	socket.emit("chat-typing", {
+		user: userEl.value,
+		message: "Is typing.......",
+	});
 });
 
 messageForm.addEventListener("submit", (e) => {
@@ -14,4 +59,18 @@ messageForm.addEventListener("submit", (e) => {
 	if (!messageEl.value) {
 		return;
 	}
+
+	if (!userEl.value) {
+		alert("You need to enter a nickname");
+		return;
+	}
+	//send message to server
+	socket.emit("chat-message", {
+		user: userEl.value,
+		message: messageEl.value,
+	});
+
+	//clear message
+	messageEl.value = "";
+	messageEl.focus();
 });
