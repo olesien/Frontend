bc2ebc79092eb12764457d8b14044dd24392c872
olesien <olesien2@gmail.com>
2022-03-16 13:48:17 +0100

    Added mongo variant

---
+++ dynamisk-webb/chat - mongo/controllers/socket_controller.js
@@ -0,0 +1,109 @@
+/**
+ * Socket Controller
+ */
+
+const debug = require('debug')('chat:socket_controller');
+const models = require('../models');
+
+let io = null; // socket.io server instance
+
+// list of rooms and their connected users
+const users = {}
+const rooms = [
+	{
+		id: 'general',
+		name: 'General',
+		users: {},
+	},
+	{
+		id: 'major',
+		name: 'Major',
+		users: {},
+	},
+	{
+		id: 'sergant',
+		name: 'Sergant',
+		users: {},
+	},
+];
+
+const handleDisconnect = function() {
+	debug(`Client ${this.id} disconnected :(`);
+
+	// find the room that this socket is part of
+	const room = rooms.find(chatroom => chatroom.users.hasOwnProperty(this.id));
+
+	// if socket was not in a room, don't broadcast disconnect
+	if (!room) {
+		return;
+	}
+
+	// let everyone in the room know that this user has disconnected
+	this.broadcast.to(room.id).emit('user:disconnected', room.users[this.id]);
+
+	// remove user from list of users in that room
+	delete room.users[this.id];
+
+	// broadcast list of users in room to all connected sockets EXCEPT ourselves
+	this.broadcast.to(room.id).emit('user:list', room.users);
+}
+
+// Handle when a user has joined the chat
+const handleUserJoined = async function(username, room_id, callback) {
+	debug(`User ${username} with socket id ${this.id} wants to join room '${room_id}'`);
+
+	// join room
+	this.join(room_id);
+
+	// add socket to list of online users in this room
+	// a) find room object with `id` === `general`
+	const room = rooms.find(chatroom => chatroom.id === room_id)
+
+	// b) add socket to room's `users` object
+	room.users[this.id] = username;
+
+	// let everyone know that someone has connected to the chat
+	this.broadcast.to(room.id).emit('user:connected', username);
+
+	// get all messages from the database
+	const messages = await models.Message.find();
+
+	// confirm join
+	callback({
+		success: true,
+		roomName: room.name,
+		messages,
+		users: room.users
+	});
+
+	// broadcast list of users in room to all connected sockets EXCEPT ourselves
+	this.broadcast.to(room.id).emit('user:list', room.users);
+}
+
+const handleChatMessage = async function(data) {
+	debug('Someone said something: ', data);
+
+	// emit `chat:message` event to everyone EXCEPT the sender
+	this.broadcast.to(data.room).emit('chat:message', data);
+
+	// save message in database
+	const message = new models.Message(data);
+	await message.save();
+}
+
+module.exports = function(socket, _io) {
+	io = _io;
+
+	debug('a new client has connected', socket.id);
+
+	io.emit("new-connection", "A new user connected");
+
+	// handle user disconnect
+	socket.on('disconnect', handleDisconnect);
+
+	// handle user joined
+	socket.on('user:joined', handleUserJoined);
+
+	// handle user emitting a new message
+	socket.on('chat:message', handleChatMessage);
+}
---
+++ dynamisk-webb/chat - mongo/public/assets/js/chat.js
@@ -0,0 +1,159 @@
+const socket = io();
+
+const startEl = document.querySelector('#start');
+const chatWrapperEl = document.querySelector('#chat-wrapper');
+const usernameForm = document.querySelector('#username-form');
+const messagesEl = document.querySelector('#messages'); // ul element containing all messages
+const messageForm = document.querySelector('#message-form');
+const messageEl = document.querySelector('#message');
+
+let room = null;
+let username = null;
+
+const addMessageToChat = (message, ownMsg = false) => {
+	// create new `li` element
+	const liEl = document.createElement('li');
+
+	// set class of `li` to `message`
+	liEl.classList.add('message');
+
+	if (ownMsg) {
+		liEl.classList.add('you');
+	}
+
+	// get human readable time
+	const time = moment(message.timestamp).format('HH:mm:ss');
+
+	// set content of `li` element
+	liEl.innerHTML = ownMsg
+		? message.content
+		: `<span class="user">${message.username}</span><span class="content">${message.content}</span><span class="time">${time}</span>`;
+
+	// append `li` element to `#messages`
+	messagesEl.appendChild(liEl);
+
+	// scroll `li` element into view
+	liEl.scrollIntoView();
+}
+
+const addNoticeToChat = notice => {
+	const liEl = document.createElement('li');
+	liEl.classList.add('notice');
+
+	liEl.innerText = notice;
+
+	messagesEl.appendChild(liEl);
+	liEl.scrollIntoView();
+}
+
+// update user list
+const updateUserList = users => {
+	document.querySelector('#online-users').innerHTML =
+		Object.values(users).map(username => `<li><span class="fa-solid fa-user-astronaut"></span> ${username}</li>`).join("");
+}
+
+// listen for when a new user connects
+socket.on('user:connected', (username) => {
+	addNoticeToChat(`${username} connected 🥳`);
+});
+
+// listen for when a user disconnects
+socket.on('user:disconnected', (username) => {
+	addNoticeToChat(`${username} disconnected 😢`);
+});
+
+// listen for when we receive an updated list of online users (in this room)
+socket.on('user:list', users => {
+	updateUserList(users);
+})
+
+// listen for when we're disconnected
+socket.on('disconnect', (reason) => {
+	if (reason === 'io server disconnect') {
+		// reconnect to the server
+		socket.connect();
+	}
+	addNoticeToChat(`You were disconnected. Reason: ${reason} 😳`);
+});
+
+// listen for when we're reconnected
+socket.io.on('reconnect', () => {
+	// join room? but only if we were in the chat previously
+	if (username) {
+		socket.emit('user:joined', username, room, (status) => {
+			addNoticeToChat(`You reconnected 🥳`);
+		});
+	}
+});
+
+// listen for incoming messages
+socket.on('chat:message', message => {
+	console.log("Someone said something:", message);
+
+	addMessageToChat(message);
+});
+
+// get username and room from form and emit `user:joined` and then show chat
+usernameForm.addEventListener('submit', e => {
+	e.preventDefault();
+
+	room = usernameForm.room.value;
+	username = usernameForm.username.value;
+
+	console.log(`User ${username} wants to join room '${room}'`);
+
+	// emit `user:joined` event and when we get acknowledgement, THEN show the chat
+	socket.emit('user:joined', username, room, (status) => {
+		// we've received acknowledgement from the server
+		console.log("Server acknowledged that user joined", status);
+
+		if (status.success) {
+			// render message history
+			status.messages.forEach(message => {
+				const ownMsg = message.username === username;
+				addMessageToChat(message, ownMsg);
+			});
+
+			// hide start view
+			startEl.classList.add('hide');
+
+			// show chat view
+			chatWrapperEl.classList.remove('hide');
+
+			// set room name as chat title
+			document.querySelector('#chat-title').innerText = status.roomName;
+
+			// focus on inputMessage
+			messageEl.focus();
+
+			// update list of users in room
+			updateUserList(status.users);
+		}
+	});
+});
+
+// send message to server
+messageForm.addEventListener('submit', e => {
+	e.preventDefault();
+
+	if (!messageEl.value) {
+		return;
+	}
+
+	const msg = {
+		username,
+		room,
+		content: messageEl.value,
+		timestamp: Date.now(),
+	}
+
+	// send message to server
+	socket.emit('chat:message', msg);
+
+	// add message to chat
+	addMessageToChat(msg, true);
+
+	// clear message input element and focus
+	messageEl.value = '';
+	messageEl.focus();
+});
